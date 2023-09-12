3519fbfda06d5d71a6ba0aa2e0999c87983a207b
Michael Krebs <michikrbs@gmail.com>
2023-09-13 00:46:31 +0200

    feat: add api connection and format

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,9 +1,15 @@
-import type {LayoutServerLoad} from './$types';
+import type { LayoutServerLoad } from "./$types";
+import { STRAPI_HOST, API_TOKEN } from "$env/static/private";
 
-export const load = (async ({fetch, params}) => {
-    const res = await fetch('http://127.0.0.1:1338/api/footer?populate=*').catch(reason => console.log(reason));
-    if (res instanceof Response) {
-        const footer = await res.json();
-        return {footer}
-    }
+export const load = (async ({ fetch, params }) => {
+  const headers = new Headers();
+  headers.append("Authorization", `Bearer ${API_TOKEN}`);
+  const res = await fetch(`${STRAPI_HOST}/api/services?populate=*`, {
+    method: "GET",
+    headers: headers,
+  }).catch((reason) => console.log(reason));
+  if (res instanceof Response) {
+    const footer = await res.json();
+    return { footer };
+  }
 }) satisfies LayoutServerLoad;
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-    import "../app.postcss";
-    import Navbar from "$lib/components/Navbar.svelte";
-    import Footer from "$components/Footer.svelte";
-    import type {LayoutData} from './$types';
+  import "../app.postcss";
+  import Navbar from "$lib/components/Navbar.svelte";
+  import Footer from "$components/Footer.svelte";
+  import type { LayoutData } from "./$types";
 
-    export let data: LayoutData;
-    console.log(data.footer.data.attributes)
+  export let data: LayoutData;
+  console.log(data)
 </script>
 
-<Navbar/>
-<slot/>
-<Footer footerData={data.footer.data.attributes}/>
+<Navbar />
+<slot />
