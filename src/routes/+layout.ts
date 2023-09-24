6f18787b8c41f1e3638faa260e6a0d6ea14dfb90
Michael Krebs <michikrbs@gmail.com>
2023-09-24 20:26:15 +0200

    fix: try build

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,11 +1,11 @@
-import { API_TOKEN, STRAPI_HOST } from "$env/static/private";
-import type { LayoutServerLoad } from "./$types";
+import { PUBLIC_API_TOKEN, PUBLIC_STRAPI_HOST } from "$env/static/public";
+import type { LayoutLoad } from "./$types";
 
-export const load: LayoutServerLoad = async ({ params }) => {
+export const load: LayoutLoad = async ({ params }) => {
     const strapiHeaders = new Headers();
-    strapiHeaders.append("Authorization", `Bearer ${API_TOKEN}`);
+    strapiHeaders.append("Authorization", `Bearer ${PUBLIC_API_TOKEN}`);
     const strapiFooterResponse = await fetch(
-        `${STRAPI_HOST}/api/footer?populate=*`,
+        `${PUBLIC_STRAPI_HOST}/api/footer?populate=*`,
         {
             method: "GET",
             headers: strapiHeaders,
