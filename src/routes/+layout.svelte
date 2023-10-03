43d9980c881671d33e95649a4523379e24d455ff
Michael Krebs <michikrbs@gmail.com>
2023-10-03 18:23:24 +0200

    style: change types path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import "../app.postcss";
-  import type { LayoutData } from "../../.svelte-kit/types/src/routes/$types";
+  import type { LayoutData } from "./$types";
   import Navbar from "$lib/components/navbar/Navbar.svelte";
   import Footer from "$lib/components/footer/Footer.svelte";
 
