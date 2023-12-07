5719e0969ca3b1305c7fca076e698a22a52a51c8
Ralf Pauli <ralf.pauli15@gmail.com>
2023-12-08 00:22:07 +0100

    extract fetch call to util file

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,17 +1,7 @@
-import { PUBLIC_API_TOKEN, PUBLIC_STRAPI_HOST } from "$env/static/public";
+import { strapiFetch } from "$lib/utils/api";
 import type { LayoutLoad } from "./$types";
 
-export const load: LayoutLoad = async ({ params }) => {
-    const strapiHeaders = new Headers();
-    strapiHeaders.append("Authorization", `Bearer ${PUBLIC_API_TOKEN}`);
-    const strapiFooterResponse = await fetch(
-        `${PUBLIC_STRAPI_HOST}/api/footer?populate=*`,
-        {
-            method: "GET",
-            headers: strapiHeaders,
-        },
-    ).catch((error) => console.log(error));
-    if (strapiFooterResponse instanceof Response) {
-        return { footer: await strapiFooterResponse.json() }
-    }
+export const load: LayoutLoad = async () => {
+    const footer = await strapiFetch("api/footer?populate=*");
+    return {footer};
 };
