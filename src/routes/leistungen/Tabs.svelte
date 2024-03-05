421267c25bbafcb91a7a220108b362242fa8019c
Ralf Pauli <ralf.pauli15@gmail.com>
2024-03-05 21:10:55 +0100

    rework services-page

---
+++ src/routes/leistungen/Tabs.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let categories;
+  export let activeCategoryId;
+  export let setActiveCategory;
+</script>
+
+<div class="flex flex-col items-start">
+  {#each categories as category}
+    <button
+      class:active="{activeCategoryId === category.id}"
+      on:click="{() => setActiveCategory(category.id)}"
+    >
+      {category.name}
+    </button>
+  {/each}
+</div>
