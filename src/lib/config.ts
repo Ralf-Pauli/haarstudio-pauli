7377ceedac4250c3cacf18246e6675241202afd2
Ralf Pauli <ralf.pauli15@gmail.com>
2026-04-03 14:02:14 +0200

    commit conig

---
+++ src/lib/config.ts
@@ -0,0 +1,18 @@
+export const siteConfig = {
+    name: "Haarstudio Pauli",
+    description: "Willkommen bei Haarstudio Pauli in Deggendorf. Ihr Friseursalon für exzellente Haarschnitte, Colorationen und Styling.",
+    url: "https://haarstudio-pauli.de",
+    address: {
+        street: "Östlicher Stadtgraben 32",
+        zipcode: "94469",
+        city: "Deggendorf"
+    },
+    contact: {
+        phone: "0151 24235034",
+        email: "info@haarstudio-pauli.de"
+    },
+    social: {
+        facebook: "https://www.facebook.com/p/Haarstudio-Pauli-61553966530565/",
+        instagram: "https://www.instagram.com/haarstudiopauli/"
+    }
+};
